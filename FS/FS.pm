1e3eae905b861761f93643aa5fce14a8be5d9ed2
ivan <ivan>
1999-08-04 04:15:20 +0000

    initial h2xs

---
+++ FS/FS.pm
@@ -0,0 +1,51 @@
+package FS;
+
+use strict;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+
+require Exporter;
+
+@ISA = qw(Exporter AutoLoader);
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+@EXPORT = qw(
+	
+);
+$VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+# Autoload methods go after =cut, and are processed by the autosplit program.
+
+1;
+__END__
+# Below is the stub of documentation for your module. You better edit it!
+
+=head1 NAME
+
+FS - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use FS;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for FS was created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+A. U. Thor, a.u.thor@a.galaxy.far.far.away
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
