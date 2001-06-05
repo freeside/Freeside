e1ae1a59b53ec391cc1b9442afd4b9f1f0f31730
ivan <ivan>
2001-06-05 21:40:55 +0000

    add deb packaging foo (doesn't work yet)

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the freeside package
+#
+0 4	* * *	root	freeside_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(freeside):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="freeside" command="/usr/bin/freeside"
