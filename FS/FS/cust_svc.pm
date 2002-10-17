d6ebf4c35d7c7b7d559828bfac3744c770dbf3eb
ivan <ivan>
2002-10-17 14:59:29 +0000

    yay missing paren

--- FS/FS/cust_svc.pm
+++ FS/FS/cust_svc.pm
@@ -393,7 +393,7 @@ sub seconds_since_sqlradacct {
                 FROM radacct
                 WHERE UserName = ?
                   AND $str2time AcctStartTime ) >= ?
-                  AND ( ? - $str2time AcctStartTime ) < 86400
+                  AND ( ? - $str2time AcctStartTime ) ) < 86400
                   AND (    $str2time AcctStopTime ) = 0
                                     OR AcctStopTime IS NULL )";
     $sth = $dbh->prepare($query) or die $dbh->errstr;
