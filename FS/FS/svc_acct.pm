3774237846391a76d2d5c57ac2638f64fef40cd0
ivan <ivan>
2003-09-06 01:45:33 +0000

    fix method name

--- FS/FS/svc_acct.pm
+++ FS/FS/svc_acct.pm
@@ -1007,16 +1007,16 @@ sub attribute_since_sqlradacct {
   $self->cust_svc->attribute_since_sqlradacct(@_);
 }
 
-=item get_session_history TIMESTAMP_START TIMESTAMP_END
+=item get_session_history_sqlradacct TIMESTAMP_START TIMESTAMP_END
 
 Returns an array of hash references of this customers login history for the
 given time range.  (document this better)
 
 =cut
 
-sub get_session_history {
+sub get_session_history_sqlradacct {
   my $self = shift;
-  $self->cust_svc->get_session_history(@_);
+  $self->cust_svc->get_session_history_sqlradacct(@_);
 }
 
 =item radius_groups
