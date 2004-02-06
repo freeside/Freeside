d5005d2e29391ef9f5498626ccec4281f6a850c3
ivan <ivan>
2004-02-06 01:00:45 +0000

    add "Net 0" invoice_default_terms

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -502,7 +502,7 @@ httemplate/docs/config.html
     'section'     => 'billing',
     'description' => 'Optional default invoice term, used to calculate a due date printed on invoices.',
     'type'        => 'select',
-    'select_enum' => [ '', 'Payable upon receipt', 'Net 10', 'Net 15', 'Net 30', 'Net 45', 'Net 60' ],
+    'select_enum' => [ '', 'Payable upon receipt', 'Net 0', 'Net 10', 'Net 15', 'Net 30', 'Net 45', 'Net 60' ],
   },
 
   {
