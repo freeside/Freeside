be4f8ca23c7d32350737887144796264b90c4e9d
ivan <ivan>
2004-02-25 05:04:36 +0000

    third time's the charm

--- FS/FS/CGI.pm
+++ FS/FS/CGI.pm
@@ -215,7 +215,7 @@ sub popurl {
   my($up)=@_;
   my $cgi = &FS::UID::cgi;
   my $url = new URI::URL ( $cgi->isa('Apache') ? $cgi->uri : $cgi->url );
-  $url->query(undef);
+  $url->query();
   my(@path)=$url->path_components;
   splice @path, 0-$up;
   $url->path_components(@path);
