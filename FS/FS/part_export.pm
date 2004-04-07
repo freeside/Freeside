de16528011da925274ae502e928c7878bbd7fefa
ivan <ivan>
2004-04-07 08:34:08 +0000

    argh, the glob itself is tainted under 5.005

--- FS/FS/part_export.pm
+++ FS/FS/part_export.pm
@@ -533,9 +533,7 @@ sub export_info {
 #}
 
 foreach my $INC ( @INC ) {
-  $INC =~ /^(.*)$/;
-  my $untainted_INC = $1;
-  foreach my $file ( glob("$untainted_INC/FS/part_export/*.pm") ) {
+  foreach my $file ( glob("$INC/FS/part_export/*.pm") ) {
     warn "attempting to load export info from $file\n" if $DEBUG;
     $file =~ /\/(\w+)\.pm$/ or do {
       warn "unrecognized file in $INC/FS/part_export/: $file\n";
