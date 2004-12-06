b5ca5dd85a6b1eacdc0888aedecadb716e625ec3
ivan <ivan>
2004-12-06 08:09:03 +0000

    fix harmless "Use of uninitialized value in length" warning in latex escapes

--- FS/FS/cust_bill.pm
+++ FS/FS/cust_bill.pm
@@ -1256,7 +1256,7 @@ sub print_pdf {
 
 sub _latex_escape {
   my $value = shift;
-  $value =~ s/([#\$%&~_\^{}])( )?/"\\$1". ( length($2) ? "\\$2" : '' )/ge;
+  $value =~ s/([#\$%&~_\^{}])( )?/"\\$1". ( ( defined($2) && length($2) ) ? "\\$2" : '' )/ge;
   $value;
 }
 
