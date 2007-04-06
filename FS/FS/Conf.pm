24cd385ebf1ab6e91d02d715475d37f8c4011eac
ivan <ivan>
2007-04-06 00:37:59 +0000

    slight update for batch-enable description & add ach-spiritone format to batch-default and batch-fixed_format-CHEK config items

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -1890,7 +1890,7 @@ httemplate/docs/config.html
   {
     'key'         => 'batch-enable',
     'section'     => 'billing',
-    'description' => 'Enable credit card batching - leave disabled for real-time installations.',
+    'description' => 'Enable credit card and/or ACH batching - leave disabled for real-time installations.',
     'type'        => 'checkbox',
   },
 
@@ -1900,7 +1900,9 @@ httemplate/docs/config.html
     'description' => 'Default format for batches.',
     'type'        => 'select',
     'select_enum' => [ 'csv-td_canada_trust-merchant_pc_batch',
-                       'csv-chase_canada-E-xactBatch', 'BoM', 'PAP' ]
+                       'csv-chase_canada-E-xactBatch', 'BoM', 'PAP',
+                       'ach-spiritone',
+                    ]
   },
 
   {
@@ -1917,7 +1919,9 @@ httemplate/docs/config.html
     'section'     => 'billing',
     'description' => 'Fixed (unchangeable) format for electronic check batches.',
     'type'        => 'select',
-    'select_enum' => [ 'csv-td_canada_trust-merchant_pc_batch', 'BoM', 'PAP' ]
+    'select_enum' => [ 'csv-td_canada_trust-merchant_pc_batch', 'BoM', 'PAP',
+                       'ach-spiritone',
+                     ]
   },
 
   {
