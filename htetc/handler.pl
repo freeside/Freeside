fddc05f485550b1e624125b8101f284cc09ef7ca
ivan <ivan>
2007-12-28 12:01:37 +0000

    restore in-browser error reporting

--- htetc/handler.pl
+++ htetc/handler.pl
@@ -37,6 +37,8 @@ if ( %%%RT_ENABLED%%% ) {
 my %interp = (
   request_class        => 'HTML::Mason::Request::ApacheHandler',
   data_dir             => '%%%MASONDATA%%%',
+  error_mode           => 'output',
+  error_format         => 'html',
   ignore_warnings_expr => '.',
   comp_root            => [
                             [ 'freeside' => '%%%FREESIDE_DOCUMENT_ROOT%%%'    ],
@@ -360,6 +362,7 @@ sub handler
 #    if ( $@ ) {
 #	$RT::Logger->crit($@);
 #    }
+    warn $@ if $@;
 
     undef %session;
 
