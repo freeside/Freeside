207515697bfcc93b03227ea0b581b15cf89b1fbc
ivan <ivan>
2008-04-08 05:49:11 +0000

    debian packages!

---
+++ debian/cron.d
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the freeside package
+#
+0 0	* * *	freeside	/usr/bin/freeside-daily fs_daily
--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the freeside package
-#
-0 4	* * *	root	freeside_maintenance
