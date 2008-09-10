44b5d849d7fb3af5ea1e2812097126633add0fd4
ivan <ivan>
2008-09-10 07:55:01 +0000

    better (?) place to put display and taxation data than overloading real line items

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -522,6 +522,39 @@ sub tables_hashref {
       'index' => [ [ 'billpkgnum' ], [ 'pkgnum', 'invnum' ] ],
     },
 
+    #instead of 'duplicate',  'char', 'NULL', 1, '', '',
+    # that way we keep display vs *TOTALLY* out of the table for actual
+    #  finanancial line items
+    'cust_bill_pkg_display' => {
+      'columns' => [
+        'billpkgdisplaynum', 'serial', '', '', '', '', 
+        'billpkgnum', 'int', '', '', '', '', 
+        'section',  'varchar', 'NULL', $char_d, '', '', 
+        #override the linked real one?#'unitsetup', @money_typen, '', '', 
+        #this too?#'unitrecur', @money_typen, '', '', 
+        'post_total', 'char', 'NULL', 1, '', '',
+        'type',       'char', 'NULL', 1, '', '',
+        #any other fields we need to control this display-only line item...
+      ],
+      'primary_key' => 'billpkgdisplaynum',
+      'unique' => [],
+      'index' => [ ['billpkgnum'], ],
+    },
+
+    #and this, to break down a line item into slices for taxation purposes
+    #(instead of creating usage line items for each usage class)
+    'cust_bill_pkg_slice' => {
+      'columns' => [
+        'slicenum', 'serial', '', '', '', '', 
+        'billpkgnum', 'int', '', '', '', '',
+        'amount',  @money_typen, '', '', 
+        'classnum', 'int', '', '', '', '',
+      ],
+      'primary_key' => 'slicenum',
+      'unique'      => [],
+      'index'       => [ [ 'billpkgnum' ], [ 'classnum' ], ],
+    },
+
     'cust_credit' => {
       'columns' => [
         'crednum',  'serial', '', '', '', '', 
