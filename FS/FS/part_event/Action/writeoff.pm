8a79ead8b600494b214d7492dab043d5dfc96587
ivan <ivan>
2009-04-11 21:42:52 +0000

    migrate send_email, suspend_if_balance and credit events, RT#3905

---
+++ FS/FS/part_event/Action/writeoff.pm
@@ -0,0 +1,33 @@
+package FS::part_event::Action::writeoff;
+
+use strict;
+use base qw( FS::part_event::Action );
+
+sub description { 'Write off bad debt with a credit entry.'; }
+
+sub option_fields {
+  ( 
+    #'charge' => { label=>'Amount', type=>'money', }, # size=>7, },
+    'reasonnum' => { 'label'        => 'Reason',
+                     'type'         => 'select-reason',
+                     'reason_class' => 'R',
+                   },
+  );
+}
+
+sub default_weight { 65; }
+
+sub do_action {
+  my( $self, $cust_object ) = @_;
+
+  my $cust_main = $self->cust_main($cust_object);
+
+  my $error = $cust_main->credit( $cust_main->balance,
+                                  $self->option('reasonnum'),
+                                );
+  die $error if $error;
+
+  '';
+}
+
+1;
