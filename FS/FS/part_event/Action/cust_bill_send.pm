ed7d3ad7e00fb57f8ded643de5d5217ee64061ad
ivan <ivan>
2009-08-20 02:40:39 +0000

    eliminate unnecessary lookup

--- FS/FS/part_event/Action/cust_bill_send.pm
+++ FS/FS/part_event/Action/cust_bill_send.pm
@@ -14,9 +14,6 @@ sub default_weight { 50; }
 sub do_action {
   my( $self, $cust_bill ) = @_;
 
-  #my $cust_main = $self->cust_main($cust_bill);
-  my $cust_main = $cust_bill->cust_main;
-
   $cust_bill->send;
 }
 
