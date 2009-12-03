1ddf402f790ff71e75a1ad4a1095a0a8c1c00a60
ivan <ivan>
2009-12-03 04:22:18 +0000

    no header?

--- FS/FS/cdr/sansay.pm
+++ FS/FS/cdr/sansay.pm
@@ -8,7 +8,7 @@ use FS::cdr qw( _cdr_date_parser_maker _cdr_min_parser_maker );
 %info = (
   'name'          => 'Sansay VSX',
   'weight'        => 135,
-  'header'        => 1,     #0 default, set to 1 to ignore the first line, or
+  'header'        => 0,     #0 default, set to 1 to ignore the first line, or
                             # to higher numbers to ignore that number of lines
   'type'          => 'csv', #csv (default), fixedlength or xls
   'sep_char'      => ';',   #for csv, defaults to ,
