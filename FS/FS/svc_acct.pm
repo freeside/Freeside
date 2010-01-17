1cfc78e91cd36d0fbbb3ae931298af4beb96eb1c
ivan <ivan>
2010-01-17 02:22:49 +0000

    remove svcnum from searchable fields for svc_acct... custnum and invnum are user-visible, other database ids, not so much.  also, breaks phone number search w/current Pg, RT#7000

--- FS/FS/svc_acct.pm
+++ FS/FS/svc_acct.pm
@@ -436,13 +436,7 @@ sub search_sql {
       $class->search_sql_field('username', $string ).
     ' ) ';
   } else {
-    ' ( '.
-      $class->search_sql_field('username', $string).
-      ( $string =~ /^\d+$/
-          ? 'OR '. $class->search_sql_field('svcnum', $string)
-          : ''
-      ).
-    ' ) ';
+    $class->search_sql_field('username', $string).
   }
 }
 
