072a39e8f246c4964f28d70265355d23def05bd9
ivan <ivan>
2010-04-04 22:52:42 +0000

    fix otaker upgrade for cust_main

--- FS/FS/cust_main.pm
+++ FS/FS/cust_main.pm
@@ -9795,6 +9795,7 @@ sub _upgrade_data { #class method
   my $sth = dbh->prepare($sql) or die dbh->errstr;
   $sth->execute or die $sth->errstr;
 
+  local($ignore_expired_card) = 1
   $class->_upgrade_otaker(%opts);
 
 }
