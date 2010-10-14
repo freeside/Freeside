3d301812d64b72b65c651fb8cbf81b6f9e65dc93
jeff <jeff>
2010-10-14 20:45:35 +0000

    restore update_censustract RT10221

--- httemplate/edit/cust_main/bottomfixup.js
+++ httemplate/edit/cust_main/bottomfixup.js
@@ -54,6 +54,79 @@ function post_geocode() {
 
 }
 
+var set_censustract;
+
+function update_censustract(arg) {
+
+  var argsHash = eval('(' + arg + ')');
+
+  var cf = document.CustomerForm;
+
+  var msacode    = argsHash['msacode'];
+  var statecode  = argsHash['statecode'];
+  var countycode = argsHash['countycode'];
+  var tractcode  = argsHash['tractcode'];
+  var error      = argsHash['error'];
+  
+  var newcensus = 
+    new String(statecode)  +
+    new String(countycode) +
+    new String(tractcode).replace(/\s$/, '');  // JSON 1 workaround
+
+  set_censustract = function () {
+
+    cf.elements['censustract'].value = newcensus
+    cf.submit();
+
+  }
+
+  if (error || cf.elements['censustract'].value != newcensus) {
+    // popup an entry dialog
+
+    if (error) { newcensus = error; }
+    newcensus.replace(/.*ndefined.*/, 'Not found');
+
+    var choose_censustract =
+      '<CENTER><BR><B>Confirm censustract</B><BR>' +
+      '<A href="http://maps.ffiec.gov/FFIECMapper/TGMapSrv.aspx?' +
+      'census_year=<% $conf->config('census_year') || '2008' %>' +
+      '&latitude=' + cf.elements['latitude'].value +
+      '&longitude=' + cf.elements['longitude'].value +
+      '" target="_blank">Map service module location</A><BR>' +
+      '<A href="http://maps.ffiec.gov/FFIECMapper/TGMapSrv.aspx?' +
+      'census_year=<% $conf->config('census_year') || '2008' %>' +
+      '&zip_code=' + cf.elements['ship_zip'].value +
+      '" target="_blank">Map zip code center</A><BR><BR>' +
+      '<TABLE>';
+    
+    choose_censustract = choose_censustract + 
+      '<TR><TH style="width:50%">Entered census tract</TH>' +
+        '<TH style="width:50%">Calculated census tract</TH></TR>' +
+      '<TR><TD>' + cf.elements['censustract'].value +
+        '</TD><TD>' + newcensus + '</TD></TR>' +
+        '<TR><TD>&nbsp;</TD><TD>&nbsp;</TD></TR>';
+
+    choose_censustract = choose_censustract +
+      '<TR><TD ALIGN="center">' +
+        '<BUTTON TYPE="button" onClick="document.CustomerForm.submit();"><IMG SRC="<%$p%>images/error.png" ALT=""> Use entered census tract </BUTTON>' + 
+      '</TD><TD ALIGN="center">' +
+        '<BUTTON TYPE="button" onClick="set_censustract();"><IMG SRC="<%$p%>images/tick.png" ALT=""> Use calculated census tract </BUTTON>' + 
+      '</TD></TR>' +
+      '<TR><TD COLSPAN=2 ALIGN="center">' +
+        '<BUTTON TYPE="button" onClick="document.CustomerForm.submitButton.disabled=false; parent.cClick();"><IMG SRC="<%$p%>images/cross.png" ALT=""> Cancel submission</BUTTON></TD></TR>' +
+        
+      '</TABLE></CENTER>';
+
+    overlib( choose_censustract, CAPTION, 'Confirm censustract', STICKY, AUTOSTATUSCAP, CLOSETEXT, '', MIDX, 0, MIDY, 0, DRAGGABLE, WIDTH, 576, HEIGHT, 268, BGCOLOR, '#333399', CGCOLOR, '#333399', TEXTSIZE, 3 );
+
+  } else {
+
+    cf.submit();
+
+  }
+
+}
+
 function copyelement(from, to) {
   if ( from == undefined ) {
     to.value = '';
