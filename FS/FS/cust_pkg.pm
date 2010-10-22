553ab4e1cb817bcf80c9e11927709b57477b0bed
ivan <ivan>
2010-10-22 09:03:46 +0000

    tealish for not yet billed, like cust_main ordered

--- FS/FS/cust_pkg.pm
+++ FS/FS/cust_pkg.pm
@@ -1812,7 +1812,7 @@ Class method that returns the list of possible status strings for packages
 =cut
 
 tie my %statuscolor, 'Tie::IxHash', 
-  'not yet billed'  => '000000',
+  'not yet billed'  => '009999', #teal? cyan?
   'one-time charge' => '000000',
   'active'          => '00CC00',
   'suspended'       => 'FF9900',
