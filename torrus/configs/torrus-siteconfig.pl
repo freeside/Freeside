113921141d9f43b97083fad9a03165c36f56904b
ivan <ivan>
2010-12-30 07:14:23 +0000

    torrus, RT#10574

--- torrus/configs/torrus-siteconfig.pl
+++ torrus/configs/torrus-siteconfig.pl
@@ -22,5 +22,11 @@ $Torrus::CGI::authorizeUsers = 0;
 $Torrus::Renderer::rendererURL = '/freeside/torrus';
 $Torrus::Renderer::plainURL    = '/freeside/torrus/plain/';
 $Torrus::Renderer::Freeside::FSURL = '%%%FREESIDE_URL%%%';
+$Torrus::Renderer::displayReports = 1;
+push (@Torrus::Collector::loadModules, 'Torrus::Collector::ExternalStorage');
+$Torrus::SQL::connections{'Default'}{'dsn'} =
+      'DBI:mysql:dbname=freeside'; #XXX sub in DATASOURCE
+$Torrus::SQL::connections{'Default'}{'username'} = 'freeside'; #DB_USER
+$Torrus::SQL::connections{'Default'}{'password'} = '';         #DB_PASSWORD
 
 1;
