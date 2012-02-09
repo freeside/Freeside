bfa0ea843311c0adfd9cb4179a307b365f157015
ivan <ivan>
2012-02-09 05:11:11 +0000

    fix census tract coding, broken by 1/17/2012 update to FFIEC site, RT#16369

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -3757,7 +3757,7 @@ and customer address. Include units.',
     'section'     => 'UI',
     'description' => 'The year to use in census tract lookups',
     'type'        => 'select',
-    'select_enum' => [ qw( 2010 2009 2008 ) ],
+    'select_enum' => [ qw( 2012 2011 2010 ) ],
   },
 
   {
