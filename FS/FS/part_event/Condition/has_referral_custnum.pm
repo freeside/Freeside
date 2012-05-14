8d159e2eda65f2e7b327872580502f37d006c7f8
Ivan Kohler <ivan@freeside.biz>
2012-05-14 13:14:06 -0700

    add balance_age_under condition and similar referring customer functionality to has_referral_custnum condition, RT#17640

--- FS/FS/part_event/Condition/has_referral_custnum.pm
+++ FS/FS/part_event/Condition/has_referral_custnum.pm
@@ -13,30 +13,49 @@ sub option_fields {
                   'type'  => 'checkbox',
                   'value' => 'Y',
                 },
+    'check_bal' => { 'label' => 'Check referring custoemr balance',
+                     'type'  => 'checkbox',
+                     'value' => 'Y',
+                   },
+    'balance' => { 'label'      => 'Referring customer balance under (or equal to)',
+                   'type'       => 'money',
+                   'value'      => '0.00', #default
+                 },
+    'age'     => { 'label'      => 'Referring customer balance age',
+                   'type'       => 'freq',
+                 },
   );
 }
 
 sub condition {
-  my($self, $object) = @_;
+  my($self, $object, %opt) = @_;
 
   my $cust_main = $self->cust_main($object);
 
   if ( $self->option('active') ) {
-
     return 0 unless $cust_main->referral_custnum;
-
     #check for no cust_main for referral_custnum? (deleted?)
-
-    $cust_main->referral_custnum_cust_main->status eq 'active';
-
+    return 0 unless $cust_main->referral_custnum_cust_main->status eq 'active';
   } else {
-
-    $cust_main->referral_custnum; # ? 1 : 0;
-
+    return 0 unless $cust_main->referral_custnum; # ? 1 : 0;
   }
+
+  return 1 unless $self->option('check_bal');
+
+  my $referring_cust_main = $cust_main->referral_custnum_cust_main;
+
+  #false laziness w/ balance_age_under
+  my $under = $self->option('balance');
+  $under = 0 unless length($under);
+
+  my $age = $self->option_age_from('age', $opt{'time'} );
+
+  $referring_cust_main->balance_date($age) <= $under;
 
 }
 
+#this is incomplete wrt checking referring customer balances, but that's okay.
+# false positives are acceptable here, its just an optimizaiton
 sub condition_sql {
   my( $class, $table ) = @_;
 
