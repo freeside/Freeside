9c021c0763dd09b8457d16389d693fdfe858ca82
Ivan Kohler <ivan@freeside.biz>
2012-06-02 16:46:04 -0700

    keep version in FS.pm instead of Makefile

--- FS/FS.pm
+++ FS/FS.pm
@@ -3,7 +3,7 @@ package FS;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '%%%VERSION%%%';
+$VERSION = '3.0git';
 
 #find missing entries in this file with:
 # for a in `ls *pm | cut -d. -f1`; do grep 'L<FS::'$a'>' ../FS.pm >/dev/null || echo "missing $a" ; done
