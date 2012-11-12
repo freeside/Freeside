1b0e3600f2004f0977c9906b3f7db56f3ca80f5d
Ivan Kohler <ivan@freeside.biz>
2012-11-11 22:34:22 -0800

    fix XSS

--- FS/FS/ClientAPI/MyAccount.pm
+++ FS/FS/ClientAPI/MyAccount.pm
@@ -2004,6 +2004,9 @@ sub _usage_details {
     $p->{ending}    = $end;
   }
 
+  die "illegal beginning" if $beginning !~ /^\d*$/;
+  die "illegal ending"    if $ending    !~ /^\d*$/;
+
   my (@usage) = &$callback($svc_x, $p->{beginning}, $p->{ending}, 
     %callback_opt
   );
