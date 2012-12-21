d14751b1bf9660a35361d25ed3bb6fc42187fcf4
Ivan Kohler <ivan@freeside.biz>
2012-12-20 19:34:06 -0800

    fix tax-on-tax (i.e. FEDERAL UNIVERSAL SERVICE FUND) on 2.3 branch, RT#20707

--- FS/FS/cust_main/Billing.pm
+++ FS/FS/cust_main/Billing.pm
@@ -799,7 +799,7 @@ sub calculate_taxes {
   my %packagemap = map { $_->pkgnum => $_ } @$cust_bill_pkg;
   foreach my $tax ( keys %$taxlisthash ) {
     foreach ( @{ $taxlisthash->{$tax} }[1 .. scalar(@{ $taxlisthash->{$tax}}) - 1] ) {
-      next unless ref($_) eq 'FS::cust_bill_pkg';
+      next unless ref($_) eq 'FS::cust_bill_pkg'; #IS needed for CCH tax-on-tax
 
       my @cust_tax_exempt_pkg = splice( @{ $_->_cust_tax_exempt_pkg } );
 
