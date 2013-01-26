55e2b73fce4db4aa189b1c102a798c4f7060d109
Ivan Kohler <ivan@freeside.biz>
2013-01-26 13:29:40 -0800

    update the redunant fallback default for 477 reports to 2012 ffiec (2010 census)

--- FS/FS/Misc/Geo.pm
+++ FS/FS/Misc/Geo.pm
@@ -81,7 +81,7 @@ sub get_censustract_ffiec {
 
       my($zip5, $zip4) = split('-',$location->{zip});
 
-      $year ||= '2011'; #2012 per http://transition.fcc.gov/form477/techfaqs.html soon/now?
+      $year ||= '2012';
       my @ffiec_args = (
         __VIEWSTATE => $viewstate,
         __EVENTVALIDATION => $eventvalidation,
