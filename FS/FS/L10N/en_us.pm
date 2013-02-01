f7b496e18b998a6eae027e56dc4943d0d6cc9c4b
Ivan Kohler <ivan@freeside.biz>
2013-01-31 21:26:55 -0800

    fix using the message catalog for en_us, RT#19907

--- FS/FS/L10N/en_us.pm
+++ FS/FS/L10N/en_us.pm
@@ -1,6 +1,8 @@
 package FS::L10N::en_us;
-use base qw(FS::L10N);
+use base qw(FS::L10N::DBI);
 
-our %Lexicon = ( _AUTO=>1 );
+#prevents english "translation" via FS::L10N::DBI, FS::Msgcat::_gettext already
+# does the same sort of fallback 
+#our %Lexicon = ( _AUTO=>1 );
 
 1;
