8232bafea84d89d7868c886821a06ac3b0c8e7c9
Ivan Kohler <ivan@freeside.biz>
2013-07-24 14:35:10 -0700

    allow duplicate contact email addresses for different contacts, RT#24148

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -1195,7 +1195,7 @@ sub tables_hashref {
         'emailaddress',   'varchar', '', $char_d, '', '',
       ],
       'primary_key' => 'contactemailnum',
-      'unique'      => [ [ 'emailaddress' ], ],
+      'unique'      => [ [ 'contactnum', 'emailaddress' ], ],
       'index'       => [],
     },
 
