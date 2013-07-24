759c31ea0334763f69c1f8bad4439f41b10d40dd
Mark Wells <mark@freeside.biz>
2013-07-24 12:56:21 -0700

    NG selfservice webpay interface, #23579

--- FS/FS/ClientAPI_XMLRPC.pm
+++ FS/FS/ClientAPI_XMLRPC.pm
@@ -122,6 +122,8 @@ sub ss2clientapi {
   'process_payment_change_pkg' => 'MyAccount/process_payment_change_pkg',
   'process_payment_order_renew' => 'MyAccount/process_payment_order_renew',
   'process_prepay'            => 'MyAccount/process_prepay',
+  'start_thirdparty'          => 'MyAccount/start_thirdparty',
+  'finish_thirdparty'         => 'MyAccount/finish_thirdparty',
   'realtime_collect'          => 'MyAccount/realtime_collect',
   'list_pkgs'                 => 'MyAccount/list_pkgs',     #add to ss (added?)
   'list_svcs'                 => 'MyAccount/list_svcs',     #add to ss (added?)
