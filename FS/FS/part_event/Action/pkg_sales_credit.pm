6891db8c9287fa670e5f6961c8ad3f2e3097fc3c
Ivan Kohler <ivan@freeside.biz>
2013-08-18 19:58:17 -0700

    continue sales person work: customer and package selection, commissions, reporting.  RT#23402

---
+++ FS/FS/part_event/Action/pkg_sales_credit.pm
@@ -0,0 +1,41 @@
+package FS::part_event::Action::pkg_sales_credit;
+
+use strict;
+use base qw( FS::part_event::Action::pkg_referral_credit );
+
+sub description { 'Credit the sales person a specific amount'; }
+
+#a little false laziness w/pkg_referral_credit
+sub do_action {
+  my( $self, $cust_pkg, $cust_event ) = @_;
+
+  my $cust_main = $self->cust_main($cust_pkg);
+
+  my $sales = $cust_pkg->sales;
+  return "No customer record for sales person ". $sales->salesperson
+    unless $sales->sales_custnum;
+
+  my $sales_cust_main = $sales->sales_cust_main;
+    #? or return "No customer record for sales person ". $sales->salesperson;
+
+  my $amount = $self->_calc_credit($cust_pkg);
+  return '' unless $amount > 0;
+
+  my $reasonnum = $self->option('reasonnum');
+
+  my $error = $sales_cust_main->credit(
+    $amount, 
+    \$reasonnum,
+    'eventnum' => $cust_event->eventnum,
+    'addlinfo' => 'for customer #'. $cust_main->display_custnum.
+                               ': '.$cust_main->name.
+                  ', package #'. $cust_pkg->pkgnum,
+    #'commission_salesnum' => $sales->salesnum,
+  );
+  die "Error crediting customer ". $sales_cust_main->custnum.
+      " for sales commission: $error"
+    if $error;
+
+}
+
+1;
