34725fae4e3c05e7d483b9379da72f0a91765106
Ivan Kohler <ivan@freeside.biz>
2013-09-26 16:36:51 -0700

    2.3.8

--- FS/FS.pm
+++ FS/FS.pm
@@ -3,7 +3,7 @@ package FS;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '2.3.8git';
+$VERSION = '2.3.8';
 
 #find missing entries in this file with:
 # for a in `ls *pm | cut -d. -f1`; do grep 'L<FS::'$a'>' ../FS.pm >/dev/null || echo "missing $a" ; done
