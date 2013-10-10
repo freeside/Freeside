85628d587669129e6595400a7db855c61dd570e9
Ivan Kohler <ivan@freeside.biz>
2013-10-09 22:26:18 -0700

    fix RT per-transaction recipient squelching, RT#25260

--- rt/lib/RT/Interface/Web.pm
+++ rt/lib/RT/Interface/Web.pm
@@ -1930,6 +1930,9 @@ is true.
 
 =cut
 
+# change from stock: if txn custom fields are set but there's no content
+# or attachment, create a Touch txn instead of doing nothing
+
 sub ProcessUpdateMessage {
 
     my %args = (
