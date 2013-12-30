6b4a2501a75964c864467a3bf85bbba039009049
Ivan Kohler <ivan@freeside.biz>
2013-12-30 01:34:13 -0800

    self-service access for contacts, RT#25533

---
+++ FS/FS/msg_template/InitialData.pm
@@ -0,0 +1,27 @@
+package FS::msg_template::InitialData;
+
+sub _initial_data {
+  [
+    { msgname   => 'Password reset',
+      mime_type => 'text/html', #multipart/alternative with a text part?
+                                  # cranky mutt/pine users like me are rare
+
+      _conf        => 'selfservice-password_reset_msgnum',
+      _insert_args => [ subject => '{ $company_name } password reset',
+                        body    => <<'END',
+To complete your { $company_name } password reset, please go to
+<a href="{ $selfservice_server_base_url }/selfservice.cgi?action=process_forgot_password;session_id={ $session_id }">{ $selfservice_server_base_url }/selfservice.cgi?action=process_forgot_password;session_id={ $session_id }</a><br />
+<br />
+This link will expire in 24 hours.<br />
+<br />
+If you did not request this password reset, you may safely ignore and delete this message.<br />
+<br />
+<br />
+{ $company_name } Support
+END
+                      ],
+    },
+  ];
+}
+
+1;
