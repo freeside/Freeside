e3fc2789a074dc4a64bc16651f72b343b4d3a4c4
Ivan Kohler <ivan@freeside.biz>
2013-12-30 14:27:51 -0800

    unique contact emails, RT#25533

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -1251,7 +1251,8 @@ sub tables_hashref {
         'emailaddress',   'varchar', '', $char_d, '', '',
       ],
       'primary_key' => 'contactemailnum',
-      'unique'      => [ [ 'contactnum', 'emailaddress' ], ],
+      #'unique'       => [ [ 'contactnum', 'emailaddress' ], ],
+      'unique'       => [ [ 'emailaddress' ], ],
       'index'       => [],
     },
 
