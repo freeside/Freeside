82380ef8cb6e506f63dd4a8d1abf89cb079ae6cc
Mark Wells <mark@freeside.biz>
2014-01-29 18:34:27 -0800

    fix localization cache, #27276

--- FS/FS/L10N/DBI.pm
+++ FS/FS/L10N/DBI.pm
@@ -3,15 +3,22 @@ use base qw(FS::L10N);
 use strict;
 use FS::Msgcat;
 
-our %Lexicon = ();
+sub lexicon {
+  my $lh = shift;
+  my $class = ref($lh) || $lh;
+  no strict 'refs';
+  \%{ $class . '::Lexicon' };
+}
 
 sub maketext {
   my($lh, $key, @rest) = @_;
 
-  unless ( exists $Lexicon{$key} ) {
-    my $lang = $lh->language_tag;
-    $lang =~ s/-(\w*)/_\U$1/;
-    $Lexicon{$key} = FS::Msgcat::_gettext( $key, $lang );
+  my $lang = $lh->language_tag;
+  $lang =~ s/-(\w*)/_\U$1/;
+
+  my $lex = $lh->lexicon;
+  unless ( exists $lex->{$key} ) {
+    $lex->{$key} = FS::Msgcat::_gettext( $key, $lang );
   }
 
   my $res = eval { $lh->SUPER::maketext($key, @rest) };
