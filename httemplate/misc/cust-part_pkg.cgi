freeside/Freeside

commit 65d0561c4d456c2d600acb03a675549d098f5776

improve performance of package ordering, RT#24955, fallout from future start dates, RT#20686

httemplate/misc/cust-part_pkg.cgi
@@ -3,7 +3,6 @@
 
 my( $custnum, $prospectnum, $classnum ) = $cgi->param('arg');
 
-
 my $agent;
 my $cust_main;
 if ( $custnum ) {
@@ -32,10 +31,17 @@ my @part_pkg = qsearch({
   'order_by'  => 'ORDER BY pkg',
 });
 
-my $date_format = FS::Conf->new->config('date_format') || '%m/%d/%Y';
+my $conf = new FS::Conf;
+
+my $date_format = $conf->config('date_format') || '%m/%d/%Y';
+
+my $default_start_date = $conf->exists('order_pkg-no_start-date')
+                           ? ''
+                           : $cust_main->next_bill_date;
 
 my @return = map  {
-                    my $start_date = $_->default_start_date($cust_main);
+                    my $start_date = $_->delay_start_date
+                                   || $default_start_date;
                     $start_date = time2str($date_format, $start_date)
                       if $start_date;
                     ( $_->pkgpart,

FS/FS/part_pkg.pm
@@ -1089,32 +1089,15 @@ sub can_discount { 0; }
 # whether the plan allows changing the start date
 sub can_start_date { 1; }
   
-# the default start date; takes an FS::cust_main as an argument
-sub default_start_date {
+# the delay start date if present
+sub delay_start_date {
   my $self = shift;
-  my $cust_main = shift;
-  my $conf = FS::Conf->new;
 
-  if ( $self->delay_start ) {
-    my $delay = $self->delay_start;
+  my $delay = $self->delay_start or return '';
     
-    my ($mday,$mon,$year) = (localtime(time))[3,4,5];
-    my $start_date = timelocal(0,0,0,$mday,$mon,$year) + 86400 * $delay;
-    return $start_date;
+  my ($mday,$mon,$year) = (localtime(time))[3,4,5];
+  timelocal(0,0,0,$mday,$mon,$year) + 86400 * $delay;
 
-  } elsif ( $conf->exists('order_pkg-no_start_date') ) {
-
-    return '';
-
-  } elsif ( $cust_main ) {
-    
-    return $cust_main->next_bill_date;
-  
-  } else {
-    
-    return '';
-
-  }
 }
 
 sub can_currency_exchange { 0; }

commit 0f5f7222dd6607c11e50020e0f6b4d813fc64cb9

fix adding packages to quotations, RT#26667, fallout from 65d0561c4d456c2d600acb03a675549d098f5776 (RT#24955 / RT#20686)

httemplate/misc/cust-part_pkg.cgi
@@ -37,7 +37,8 @@ my $date_format = $conf->config('date_format') || '%m/%d/%Y';
 
 my $default_start_date = $conf->exists('order_pkg-no_start_date')
                            ? ''
-                           : $cust_main->next_bill_date;
+                           : $cust_main ? $cust_main->next_bill_date
+                                        : '';
 
 #num_billing_pkgs may be slightly better (will allow you to fill in a start
 # date in the weird edge case where you're using sync_next_bill and