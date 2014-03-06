0d991a8c986099180aaf2843a928e9396ff232db
Joshua Goodman <jgoodman1990@gmail.com>
2014-03-06 22:45:53 +0000

    modified _upgrade to grant "Add on-the-fly refund reason" to groups with "Add on-the-fly credit reason"

--- FS/FS/access_right.pm
+++ FS/FS/access_right.pm
@@ -243,7 +243,7 @@ sub _upgrade_data { # class method
     'Configuration' => 'Alarm global configuration',
     'Services: Accounts' => 'Services: Conferencing',
     'Services: Accounts' => 'Services: Video',
-    'Edit global package definitions' => 'Edit package definition costs',
+    'Add on-the-fly credit reason' => 'Add on-the-fly refund reason',
   );
 
 #  foreach my $old_acl ( keys %onetime ) {
