2b4ca5eec4564db2df08da6fa71592d010343783
Ivan Kohler <ivan@freeside.biz>
2014-03-05 19:33:12 -0800

    backoffice API: add location_info, RT#22830

--- FS/FS/API.pm
+++ FS/FS/API.pm
@@ -41,7 +41,6 @@ Enter cash refund.
 
 #---
 
-#generally, the more useful data from the cust_main record the better.
 
 
 # "2 way syncing" ?  start with non-sync pulling info here, then if necessary
@@ -96,9 +95,13 @@ sub customer_info {
   $return{'postal_invoicing'} =
     0 < ( grep { $_ eq 'POST' } @invoicing_list );
 
+  #generally, the more useful data from the cust_main record the better.
+  # well, tell me what you want
+
   return \%return;
 
 }
+
 
 #I also monitor for changes to the additional locations that are applied to
 # packages, and would like for those to be exportable as well.  basically the
@@ -113,7 +116,7 @@ sub location_info {
 
   my %return = (
     'error'           => '',
-    'locations'       => [ @cust_location ],
+    'locations'       => [ map $_->hashref, @cust_location ],
   );
 
   return \%return;
