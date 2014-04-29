366d5ca5d8dfe56330457a96bfc10dd036a4c63a
Mark Wells <mark@freeside.biz>
2014-04-28 23:23:32 -0700

    make cust_main location upgrade non-blocking, and make the system somewhat functional before the upgrade is complete, #28883

--- FS/FS/Cursor.pm
+++ FS/FS/Cursor.pm
@@ -2,12 +2,12 @@ package FS::Cursor;
 
 use strict;
 use vars qw($DEBUG $buffer);
-use base qw( Exporter );
-use FS::Record qw(qsearch dbdef dbh);
-use Data::Dumper;
+use FS::Record;
+use FS::UID qw(myconnect);
 use Scalar::Util qw(refaddr);
 
-$DEBUG = 0;
+$DEBUG = 2;
+
 # this might become a parameter at some point, but right now, you can
 # "local $FS::Cursor::buffer = X;"
 $buffer = 200;
@@ -39,21 +39,25 @@ and returns an FS::Cursor object to fetch the rows one at a time.
 sub new {
   my $class = shift;
   my $q = FS::Record::_query(@_); # builds the statement and parameter list
+  my $dbh = myconnect();
 
   my $self = {
     query => $q,
     class => 'FS::' . ($q->{table} || 'Record'),
     buffer => [],
+    dbh   => $dbh,
   };
   bless $self, $class;
 
   # the class of record object to return
   $self->{class} = "FS::".($q->{table} || 'Record');
 
+  # save for later, so forked children will not destroy me when they exit
+  $self->{pid} = $$;
+
   $self->{id} = sprintf('cursor%08x', refaddr($self));
   my $statement = "DECLARE ".$self->{id}." CURSOR FOR ".$q->{statement};
 
-  my $dbh = dbh;
   my $sth = $dbh->prepare($statement)
     or die $dbh->errstr;
   my $bind = 1;
@@ -103,9 +107,12 @@ sub refill {
 
 sub DESTROY {
   my $self = shift;
-  my $statement = "CLOSE ".$self->{id};
-  dbh->do($statement);
-}  
+  return unless $self->{pid} eq $$;
+  $self->{dbh}->do('CLOSE '. $self->{id})
+    or die $self->{dbh}->errstr; # clean-up the cursor in Pg
+  $self->{dbh}->rollback;
+  $self->{dbh}->disconnect;
+}
 
 =back
 
@@ -116,6 +123,12 @@ Replace all uses of qsearch with this.
 =head1 BUGS
 
 Doesn't support MySQL.
+
+The cursor will close prematurely if any code issues a rollback/commit. If
+you need protection against this use qsearch or fork and get a new dbh
+handle.
+Normally this issue will represent itself this message.
+ERROR: cursor "cursorXXXXXXX" does not exist.
 
 =head1 SEE ALSO
 
