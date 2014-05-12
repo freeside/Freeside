56845a27f564298d148f968fc074e2dea7afd8c7
Jeremy Davis <jeremyd@freeside.biz>
2014-05-12 12:51:14 -0400

    28977 VSS CDR format

---
+++ FS/FS/cdr/vss.pm
@@ -0,0 +1,35 @@
+package FS::cdr::vss;
+
+use strict;
+use vars qw( @ISA %info $tmp_mon $tmp_mday $tmp_year );
+use Time::Local;
+use FS::cdr qw(_cdr_date_parser_maker);
+
+@ISA = qw(FS::cdr);
+
+%info = (
+  'name'          => 'VSS',
+  'weight'        => 120,
+  'header'        => 1,
+  'import_fields' => [
+
+        skip(1),        # Customer
+        skip(1),        # i_customer
+        'accountcode',  # account_id
+        'src',          # caller
+        'dst',          # called
+        skip(1),        # reason
+#        _cdr_date_parser_maker('startdate'),       # time
+        'calldate',
+        'billsec',      # duration
+        skip(3),        # ringtime
+                        # status
+                        # resller_charge
+       'upstream_price',# customer_charge
+  ],
+);
+
+sub skip { map {''} (1..$_[0]) }
+
+1;
+
