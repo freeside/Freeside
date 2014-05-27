3aa289492eadafb498855bf32abca45ce284ad59
Jason Terry <jterry@jterry-mac.local>
2014-05-27 16:14:19 -0600

    Allow a custom error for when the database is down (or some other preuser_setup failure)
    With a minor edit to httemplate/loginout/login.html this will allow you to show a generic message along the lines of "Auth
    service is down.  Please try again in 5 minutes" instead of the server error page that is displayed now.

--- FS/FS/AuthCookieHandler.pm
+++ FS/FS/AuthCookieHandler.pm
@@ -9,11 +9,14 @@ use FS::Auth;
 sub authen_cred {
   my( $self, $r, $username, $password ) = @_;
 
-  preuser_setup();
+  if (!eval{preuser_setup();}) {
+      return (undef,'no_preuser_setup');
+  }
 
   my $info = {};
 
-  unless ( FS::Auth->authenticate($username, $password, $info) ) {
+  my $sessionkey;
+  unless ( $sessionkey = FS::Auth->authenticate($username, $password, $info) ) {
     warn "failed auth $username from ". $r->connection->remote_ip. "\n";
     return undef;
   }
@@ -25,19 +28,40 @@ sub authen_cred {
                               %$info,
                             );
 
-  FS::CurrentUser->new_session;
+  FS::CurrentUser->new_session($sessionkey);
+}
+
+sub custom_errors {
+    my ($self,$r,$auth_user,@args) = @_;
+    my $auth_type = $r->auth_type;
+    $auth_type->remove_cookie($r);
+    my @allowed_errors = ['no_preuser_setup'];
+    $r->subprocess_env('AuthCookieReason', $args[0] ~~ @allowed_errors ? $args[0] : 'bad_cookie');
+    $auth_type->login_form($r);
 }
 
 sub authen_ses_key {
   my( $self, $r, $sessionkey ) = @_;
 
-  preuser_setup();
+  if (!eval{preuser_setup();}) {
+      return (undef,'no_preuser_setup');
+  }
 
   my $curuser = FS::CurrentUser->load_user_session( $sessionkey );
 
   unless ( $curuser ) {
     warn "bad session $sessionkey from ". $r->connection->remote_ip. "\n";
     return undef;
+  }
+
+  my $new_sessionkey;
+  unless ( $new_sessionkey = FS::Auth->verify_user( $curuser, $sessionkey ) ) {
+    warn "bad verify_user $sessionkey from ". $r->connection->remote_ip. "\n";
+    return undef;
+  }
+  if ($new_sessionkey ne $sessionkey && $new_sessionkey ne 1) {
+    FS::CurrentUser->new_session($new_sessionkey);
+    Apache2::AuthCookie->send_cookie($r, $new_sessionkey);
   }
 
   $curuser->username;
