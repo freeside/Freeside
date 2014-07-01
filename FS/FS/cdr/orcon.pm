868c313527afb05ba7a85527bde4ccef6b85632c
Jeremy Davis <jeremyd@freeside.biz>
2014-07-01 13:42:23 -0400

    29050 trailing record

--- FS/FS/cdr/orcon.pm
+++ FS/FS/cdr/orcon.pm
@@ -13,20 +13,26 @@ use Date::Parse;
   'header'        => 1,
   'import_fields' => [
 
-        skip(2),        #id
-                        #billing period
+	skip(1)      ,  #id
+        skip(1)      ,  #billing period
         'accountcode',  #account number
         skip(2),        #username
                         #service id
-        sub { my ($cdr, $calldate) = @_;
+        sub { my ($cdr, $calldate, $param) = @_;
+        
          	$cdr->set('calldate', $calldate);
 
-                $calldate =~ /^(\d{4})-(\d{2})-(\d{2})\s*(\d{2}):(\d{2}):(\d{2})$/
-                               or die "unparseable date: $calldate";
+                if ($calldate =~ /^(\d{4})-(\d{2})-(\d{2})\s*(\d{2}):(\d{2}):(\d{2})$/){
+
                 my $tmp_date = "$2/$3/$1 $4:$5:$6";
 
                 $tmp_date = str2time($tmp_date);
                 $cdr->set('startdate', $tmp_date);
+	
+		} else {
+
+			$param->{skiprow} = 1
+		}
                   },    #date
         skip(1),        #tariff region
         'src',          #originating number
