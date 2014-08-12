fc330b9dabbf5094f791c63e44a1e25aa206abb6
Ivan Kohler <ivan@freeside.biz>
2014-08-12 12:39:02 -0700

    fix log contexts, RT#30238

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -6248,7 +6248,7 @@ sub tables_hashref {
       'columns' => [
         'logcontextnum', 'serial', '', '', '', '',
         'lognum', 'int', '', '', '', '',
-        'context', 'varchar', '', 32, '', '',
+        'context', 'varchar', '', $char_d, '', '',
       ],
       'primary_key'  => 'logcontextnum',
       'unique'       => [ [ 'lognum', 'context' ] ],
