0dd51846f6279fcdf3b4a7749b70d3b2fad8b0a5
C.J. Adams-Collier <cjac@colliertech.org>
2014-09-18 11:56:02 -0700

    FS RT #30831 - passing the ISO 3166-1 alpha-2 country code to ut_phonen

--- FS/FS/svc_alarm.pm
+++ FS/FS/svc_alarm.pm
@@ -198,13 +198,15 @@ sub check {
   my $x = $self->setfixed;
   return $x unless ref $x;
 
+  my $iso3166 = $self->cust_main->ship_location->country();
+
   my $error =
     $self->ut_numbern('svcnum')
     || $self->ut_text('acctnum')
     || $self->ut_alphan('_password')
     || $self->ut_textn('location')
     || $self->ut_numbern('cs_receiver')
-    || $self->ut_phonen('cs_phonenum')
+    || $self->ut_phonen('cs_phonenum', $iso3166)
     || $self->ut_alphan('serialnum')
     || $self->ut_foreign_key('alarmsystemnum',  'alarm_system',  'systemnum')
     || $self->ut_foreign_key('alarmtypenum',    'alarm_type',    'typenum')
