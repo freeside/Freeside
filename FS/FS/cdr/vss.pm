3503b600f300515c448d19a3e332e5c24d2bdfbe
Jeremy Davis <jeremyd@freeside.biz>
2014-11-04 08:28:40 -0500

    Ticket #31744 Update vss cdr format

--- FS/FS/cdr/vss.pm
+++ FS/FS/cdr/vss.pm
@@ -18,9 +18,9 @@ use FS::cdr qw(_cdr_date_parser_maker);
         'accountcode',  # account_id
         'src',          # caller
         'dst',          # called
-        skip(1),        # reason
-#        _cdr_date_parser_maker('startdate'),       # time
-        'calldate',
+        skip(2),        # reason
+			# call id
+        _cdr_date_parser_maker('startdate'),       # time
         'billsec',      # duration
         skip(3),        # ringtime
                         # status
