67cef14ba963b2dab906d5e67095c8d1d3de43b5
Mark Wells <mark@freeside.biz>
2014-11-06 16:37:24 -0800

    clean up existing packages when changing supplemental package defs, #31194

--- FS/FS/part_pkg_link.pm
+++ FS/FS/part_pkg_link.pm
@@ -2,8 +2,11 @@ package FS::part_pkg_link;
 
 use strict;
 use vars qw( @ISA );
-use FS::Record qw( qsearchs );
+use FS::Record qw( qsearchs qsearch dbh );
 use FS::part_pkg;
+use FS::cust_pkg;
+use FS::reason;
+use FS::reason_type;
 
 @ISA = qw(FS::Record);
 
@@ -81,17 +84,136 @@ sub table { 'part_pkg_link'; }
 Adds this record to the database.  If there is an error, returns the error,
 otherwise returns false.
 
+If this is a supplemental package link, inserting it will order the 
+supplemental packages for any main packages that already exist.
+
 =cut
 
-# the insert method can be inherited from FS::Record
+sub insert {
+  my $oldAutoCommit = $FS::UID::AutoCommit;
+  local $FS::UID::AutoCommit = 0;
+  my $dbh = dbh;
+
+  my $self = shift;
+  my $error = $self->SUPER::insert(@_);
+  if ( $error ) {
+    $dbh->rollback if $oldAutoCommit;
+    return $error if $error;
+  }
+
+  if ( $self->link_type eq 'supp' ) {
+    # queue this?
+    my @main_pkgs = qsearch('cust_pkg', {
+        pkgpart => $self->src_pkgpart,
+        cancel  => '',
+    });
+    foreach my $main_pkg (@main_pkgs) {
+      # duplicates code in FS::cust_pkg::uncancel, sort of
+      my $supp_pkg = FS::cust_pkg->new({
+          'pkgpart'     => $self->dst_pkgpart,
+          'pkglinknum'  => $self->pkglinknum,
+          'main_pkgnum' => $main_pkg->pkgnum,
+          'order_date'  => time,
+          map { $_ => $main_pkg->get($_) }
+          qw( custnum locationnum pkgbatch 
+              start_date setup expire adjourn contract_end bill susp 
+              refnum discountnum waive_setup quantity 
+              recur_show_zero setup_show_zero )
+      });
+      $error = $supp_pkg->insert;
+      if ( $error ) {
+        $dbh->rollback if $oldAutoCommit;
+        return "$error (ordering new supplemental package to pkg#".$main_pkg->pkgnum.")" if $error;
+      }
+    }
+
+    return $error if $error;
+  }
+
+  return;
+}
 
 =item delete
 
 Delete this record from the database.
 
+If this is a supplemental package link, deleting it will set pkglinknum = null
+for any related packages, and set those packages to expire on their next bill
+date.
+
 =cut
 
-# the delete method can be inherited from FS::Record
+my $cancel_reason_text = 'Supplemental package removed';
+my $cancel_reason_type = 'Cancel Reason';
+
+sub delete {
+  my $oldAutoCommit = $FS::UID::AutoCommit;
+  local $FS::UID::AutoCommit = 0;
+  my $dbh = dbh;
+
+  my $self = shift;
+
+  if ( $self->link_type eq 'supp' ) {
+    my $error = $self->remove_linked;
+    if ( $error ) {
+      $dbh->rollback if $oldAutoCommit;
+      return $error;
+    }
+  }
+
+  my $error = $self->SUPER::delete(@_);
+  if ( $error ) {
+    $dbh->rollback if $oldAutoCommit;
+    return $error;
+  }
+  $dbh->commit;
+  return;
+}
+
+sub remove_linked {
+  my $self = shift;
+  my $pkglinknum = $self->pkglinknum;
+  my $error;
+
+  # find linked packages
+  my @pkgs = qsearch('cust_pkg', { pkglinknum => $pkglinknum });
+  warn "expiring ".scalar(@pkgs).
+       " linked packages from part_pkg_link #$pkglinknum\n";
+
+  my $reason = qsearchs('reason', { reason => $cancel_reason_text });
+  if (!$reason) {
+    # upgrade/FS::Setup created this one automatically
+    my $reason_type = qsearchs('reason_type',
+                               { type => $cancel_reason_type }
+      ) or die "default cancel reason type does not exist";
+
+    $reason = FS::reason->new({
+        reason_type => $reason_type->typenum,
+        reason      => $cancel_reason_text,
+        disabled    => 'Y',
+    });
+    $error = $reason->insert;
+    if ( $error ) {
+      return "$error (creating package cancel reason)";
+    }
+  }
+
+  foreach my $pkg (@pkgs) {
+    $pkg->set('pkglinknum' => '');
+    if ( $pkg->get('cancel') ) {
+      # then just replace it to unlink the package from this object
+      $error = $pkg->replace;
+    } else {
+      $error = $pkg->cancel(
+        'date'    => $pkg->get('bill'), # cancel on next bill, or else now
+        'reason'  => $reason->reasonnum,
+      );
+    }
+    if ( $error ) {
+      return "$error (scheduling package #".$pkg->pkgnum." for expiration)";
+    }
+  }
+}
 
 =item replace OLD_RECORD
 
