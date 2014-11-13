eb496e0e972cdaa94f894d232c5cc6dfa250e9ad
Ivan Kohler <ivan@freeside.biz>
2014-11-12 20:50:17 -0800

    add option to credit customer sales person if there is no package sales person to amount based on referred package action, RT#24086

--- FS/FS/part_event/Action/pkg_sales_credit_pkg.pm
+++ FS/FS/part_event/Action/pkg_sales_credit_pkg.pm
@@ -1,7 +1,6 @@
 package FS::part_event::Action::pkg_sales_credit_pkg;
-
-use strict;
-use base qw( FS::part_event::Action::Mixin::credit_pkg
+use base qw( FS::part_event::Action::Mixin::pkg_sales_credit
+             FS::part_event::Action::Mixin::credit_pkg
              FS::part_event::Action::pkg_sales_credit );
 
 sub description { 'Credit the package sales person an amount based on the referred package'; }
