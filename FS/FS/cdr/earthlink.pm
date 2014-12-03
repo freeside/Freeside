2b9de94cafad5ea0d439ce9be9d1ca26095154bb
Jeremy Davis <jeremyd@freeside.biz>
2014-12-03 12:41:12 -0500

    Ticket #31495 Earthlink CDR

---
+++ FS/FS/cdr/earthlink.pm
@@ -0,0 +1,44 @@
+package FS::cdr::earthlink;
+
+use strict;
+use vars qw( @ISA %info $date);
+use Time::Local;
+use FS::cdr qw(_cdr_date_parser_maker _cdr_min_parser_maker);
+use Date::Parse;
+
+@ISA = qw(FS::cdr);
+
+%info = (
+  'name'          => 'Earthlink',
+  'weight'        => 120,
+  'header'        => 1,
+  'import_fields' => [
+
+	'accountcode',			#Account number
+              skip(2),  		#SERVICE LOC / BILL NUMBER 
+	sub { my($cdr, $date) = @_;  
+	
+	}, 				#date 
+	sub { my($cdr, $time) = @_;
+
+	my $datetime = $date. " ". $time;
+	$cdr->set('startdate', $datetime );
+        },              		#time
+	sub { my($cdr, $src) = @_;	
+	$src =~ s/\D//g;
+	$cdr->set('src', $src);
+	},				#ORIG NUMBER
+	skip(2),			#ORIG CITY/ORIGSTATE
+	sub { my($cdr, $dst) = @_;
+        $dst =~ s/\D//g;
+        $cdr->set('dst', $dst);
+        },				#TERM NUMBER
+	skip(2),			#TERM CITY / TERM STATE
+	_cdr_min_parser_maker, 		#MINUTES
+  ],
+);
+
+sub skip { map {''} (1..$_[0]) }
+
+1;
+
