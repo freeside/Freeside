f82ca6d06518839408050aed4812fea1ba864ad3
Ivan Kohler <ivan@freeside.biz>
2015-01-18 14:40:00 -0800

    fully-qualify

--- FS/FS/svc_Common.pm
+++ FS/FS/svc_Common.pm
@@ -1394,7 +1394,7 @@ sub search {
 
   #custnum
   if ( $params->{'custnum'} =~ /^(\d+)$/ && $1 ) {
-    push @where, "custnum = $1";
+    push @where, "cust_pkg.custnum = $1";
   }
 
   #customer status
