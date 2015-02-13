3ad1a49dc4266193bf1036b79d8223d205cfb19d
Ivan Kohler <ivan@freeside.biz>
2015-02-12 17:34:49 -0800

    brigher purple on-hold, RT#33472

--- FS/FS/cust_main/Status.pm
+++ FS/FS/cust_main/Status.pm
@@ -47,7 +47,7 @@ sub statuscolors {
 
   if ( $module eq 'Classic' ) {
     tie %statuscolors, 'Tie::IxHash',
-      'prospect'  => '7e0079', #'000000', #black?  naw, purple
+      'prospect'  => 'FF00F5', #'000000', #black?  naw, purple
       'active'    => '00CC00', #green
       'ordered'   => '009999', #teal? cyan?
       'inactive'  => '0000CC', #blue
@@ -56,7 +56,7 @@ sub statuscolors {
     ;
   } elsif ( $module eq 'Recurring' ) {
     tie %statuscolors, 'Tie::IxHash',
-      'prospect'  => '7e0079', #'000000', #black?  naw, purple
+      'prospect'  => 'FF00F5', #'000000', #black?  naw, purple
       'active'    => '00CC00', #green
       'ordered'   => '009999', #teal? cyan?
       'suspended' => 'FF9900', #yellow
