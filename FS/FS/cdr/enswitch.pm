365d3d9a429b4797fd38c5be7f555bfe272509a9
Jeremy Davis <jeremyd@freeside.biz>
2015-03-01 20:58:52 -0500

    Enswitch CDR changes

--- FS/FS/cdr/enswitch.pm
+++ FS/FS/cdr/enswitch.pm
@@ -16,20 +16,18 @@ use FS::cdr_type;
     'startdate',    #Start, already a unix timestamp
     skip(2),        #Start date, Start time
     'enddate',      #End
-    skip(4),        #End date, End time
+    skip(6),        #End date, End time
                     #Calling customer, Calling type
-    'src',          #Calling number
-    'clid',         #Calling name
+    'src',          #Calling number     
     skip(1),        #Called type
     'dst',          #Called number
-    skip(10),       #Destination customer, Destination type
+    skip(26),       #Destination customer, Destination type
                     #Destination number
                     #Destination group ID, Destination group name,
-    \&in_calling_type,  #Inbound calling type,
-    \&in_calling_num,   #Inbound calling number,
-    '',                 #Inbound called type,
-    \&in_called_num,    #Inbound called number,
-    skip(11),
+    		    #Inbound calling type,
+    		    #Inbound calling number,
+                    #Inbound called type,
+    		    #Inbound called number,
                     #Inbound destination type, Inbound destination number,
                     #Outbound calling type, Outbound calling number,
                     #Outbound called type, Outbound called number,
@@ -66,16 +64,6 @@ sub in_calling_type {
   $record->set('in_calling_type', $data); #for below
   $record->set('cdrtypenum', $cdr_type->cdrtypenum) if $cdr_type;
 
-}
-
-sub in_calling_num {
-  my ($record, $data) = @_;
-  $record->src($data) if ( ($record->in_calling_type || '') eq 'external' );
-}
-
-sub in_called_num {
-  my ($record, $data) = @_;
-  $record->dst($data) if ( ($record->in_calling_type || '') eq 'external' );
 }
 
 1;
