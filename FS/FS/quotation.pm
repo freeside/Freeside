93b55cfdc07c1c29e9b42e801713f6595eaf071f
Mark Wells <mark@freeside.biz>
2015-03-10 12:58:55 -0700

    set default itemdesc="Tax" on quotation taxes, #32489

--- FS/FS/quotation.pm
+++ FS/FS/quotation.pm
@@ -698,7 +698,7 @@ sub estimate {
 
       my $quotation_pkg_tax = FS::quotation_pkg_tax->new({
           quotationpkgnum => $pkg->quotationpkgnum,
-          itemdesc        => $tax_def->taxname,
+          itemdesc        => ($tax_def->taxname || 'Tax'),
           taxnum          => $taxnum,
           taxtype         => ref($tax_def),
       });
