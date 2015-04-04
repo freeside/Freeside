506bee9c2fefa9a3cd4415a069b1fb16d9f4e3ab
Jonathan Prykop <jonathan@freeside.biz>
2015-04-03 19:16:44 -0500

    RT#14671: Usage for current day... [added check to _make_lines]

--- FS/FS/cust_main/Billing.pm
+++ FS/FS/cust_main/Billing.pm
@@ -1044,6 +1044,7 @@ sub _make_lines {
                               )
                          )
                   )
+               || $cust_pkg->is_status_delay_cancel
            )
        and
             ( $part_pkg->freq ne '0' && ( $cust_pkg->bill || 0 ) <= $cmp_time )
