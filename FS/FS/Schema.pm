03c9f5e33216c503fb52dc02826426b17f01f629
Jonathan Prykop <jonathan@freeside.biz>
2015-04-13 17:59:12 -0500

    RT#18834 Cacti integration [schema history fix and cleanup refactor]

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -197,7 +197,7 @@ sub dbdef_dist {
            && ! /^legacy_cust_history$/
            && ( ! /^queue(_arg|_depend|_stat)?$/ || ! $opt->{'queue-no_history'} )
            && ! $tables_hashref_torrus->{$_}
-           && ! /^cacti_graph$/
+           && ! /^cacti_page$/
          }
       $dbdef->tables
   ) {
