830c02cb25147b6a7d7078b334553ea6d001af27
Jeremy Davis <jeremyd@freeside.biz>
2015-05-26 10:55:41 -0400

    VSS CDR format mis-spelled

---
+++ FS/FS/cdr/FS/FS/cdr/vvs.pm
@@ -0,0 +1,33 @@
+package FS::cdr::vvs;
+
+use strict;
+use vars qw( @ISA %info $tmp_mon $tmp_mday $tmp_year );
+use Time::Local;
+use FS::cdr qw(_cdr_date_parser_maker);
+
+@ISA = qw(FS::cdr);
+
+%info = (
+  'name'          => 'VVS',
+  'weight'        => 120,
+  'header'        => 1,
+  'import_fields' => [
+
+        skip(1),        # i_customer
+        'accountcode',  # account_id
+        'src',          # caller
+        'dst',          # called
+        skip(2),        # reason
+			# call id
+        _cdr_date_parser_maker('startdate'),       # time
+        'billsec',      # duration
+        skip(3),        # ringtime
+                        # status
+                        # resller_charge
+       'upstream_price',# customer_charge
+  ],
+);
+
+sub skip { map {''} (1..$_[0]) }
+
+1;
