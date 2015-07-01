b02738fceb2de16b23fb22a70350b19ee88a214c
Jonathan Prykop <jonathan@freeside.biz>
2015-06-30 22:30:56 -0500

    RT#20687: Future package change [bug fix]

--- FS/FS/cust_pkg.pm
+++ FS/FS/cust_pkg.pm
@@ -2442,8 +2442,9 @@ sub change_later {
 
         $error = $self->replace       ||
                  $err_or_pkg->replace ||
-                 $change_to->cancel('no_delay_cancel' => 1) ||
-                 $change_to->delete;
+                 (($err_or_pkg->pkgnum == $change_to->pkgnum) ? '' :
+                  $change_to->cancel('no_delay_cancel' => 1) ||
+                  $change_to->delete);
       } else {
         $error = $err_or_pkg;
       }
