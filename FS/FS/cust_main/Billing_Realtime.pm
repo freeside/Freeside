052eb9b8d16c4e81c0b6c609674e9ab768cb817d
Jonathan Prykop <jonathan@freeside.biz>
2015-11-04 00:44:52 -0600

    RT#38314: Declined payment shows card as tokenized after first attempt [fixed if paymask is not passed, take 2]

--- FS/FS/cust_main/Billing_Realtime.pm
+++ FS/FS/cust_main/Billing_Realtime.pm
@@ -266,7 +266,10 @@ sub _bop_defaults {
     }
   }
 
-  $options->{payinfo} = $self->payinfo unless exists( $options->{payinfo} );
+  unless ( exists( $options->{'payinfo'} ) ) {
+    $options->{'payinfo'} = $self->payinfo;
+    $options->{'paymask'} = $self->paymask;
+  }
 
   # Default invoice number if the customer has exactly one open invoice.
   if( ! $options->{'invnum'} ) {
