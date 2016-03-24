028e089bd3219330418ce40ab07926a320584e7e
Ivan Kohler <ivan@freeside.biz>
2016-03-24 10:51:07 -0700

    omit h_access_user_log, RT#41196

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -201,11 +201,9 @@ sub dbdef_dist {
     grep {    ! /^(clientapi|access_user)_session/
            && ! /^h_/
            && ! /^log(_context)?$/
-           && ! /^legacy_cust_history$/
+           && ! /^(legacy_cust_history|cacti_page|template_image|access_user_log)$/
            && ( ! /^queue(_arg|_depend|_stat)?$/ || ! $opt->{'queue-no_history'} )
            && ! $tables_hashref_torrus->{$_}
-           && ! /^cacti_page$/
-           && ! /^template_image$/
          }
       $dbdef->tables
   ) {
