73a0010272b15b42c19a40f59fd09c74fca2af5c
Ivan Kohler <ivan@freeside.biz>
2016-03-26 22:45:09 -0700

    freeside inc. web services for address normalizaion and printing, RT#33849

--- FS/FS/access_right.pm
+++ FS/FS/access_right.pm
@@ -254,6 +254,7 @@ sub _upgrade_data { # class method
     'Add on-the-fly void credit reason' => 'Add on-the-fly void reason',
     '_ALL' => 'Employee preference telephony integration',
     'Edit customer package dates' => 'Change package start date', #4.x
+    'Resend invoices' => 'Print and mail invoices',
   );
 
 #  foreach my $old_acl ( keys %onetime ) {
