f3f54e8ebae3d2f05858ab856f0b727f55c434f7
Mark Wells <mark@freeside.biz>
2016-04-13 13:18:41 -0700

    add part_referral external ID for API signups, #39776

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -3591,10 +3591,11 @@ sub tables_hashref {
         'refnum',   'serial',     '',        '', '', '', 
         'referral', 'varchar',    '',   $char_d, '', '', 
         'disabled', 'char',   'NULL',         1, '', '', 
-        'agentnum', 'int',    'NULL',        '', '', '', 
+        'agentnum', 'int',    'NULL',        '', '', '',
+        'title',   'varchar', 'NULL',   $char_d, '', '', 
       ],
       'primary_key'  => 'refnum',
-      'unique'       => [],
+      'unique'       => [ ['agentnum', 'title'] ],
       'index'        => [ ['disabled'], ['agentnum'], ],
       'foreign_keys' => [
                           { columns    => [ 'agentnum' ],
