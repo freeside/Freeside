d1f9776b822ae835ba5374dca3f76ce3ee76e5dc
Mark Wells <mark@freeside.biz>
2016-05-19 16:57:06 -0700

    fix some dangling records on upgrade, #32456 and #38765

--- FS/FS/Upgrade.pm
+++ FS/FS/Upgrade.pm
@@ -523,7 +523,9 @@ sub upgrade_schema_data {
     'cust_bill_pkg_detail' => [],
     #add necessary columns to RT schema
     'TicketSystem' => [],
-
+    #remove possible dangling records
+    'password_history' => [],
+    'cust_pay_pending' => [],
   ;
 
   \%hash;
