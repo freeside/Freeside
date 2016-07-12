fe92beb1b4322f86815a83491723b07aca2f9b93
Mark Wells <mark@freeside.biz>
2016-07-12 14:26:07 -0700

    Revert "workaround for page-break-inside:avoid on Chrome, #71301"
    This reverts commit 66eb3662102dbf92ac40fb8889c9c5051fd1ed29.

--- httemplate/elements/printtofit.js
+++ httemplate/elements/printtofit.js
@@ -1,30 +1,13 @@
 $().ready(function() {
-  // yuck
-  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
   var beforePrint = function() {
     if ($('body').width() > 0) {
       // 7.5 inches * 96 DPI; maybe make the width a user pref?
       var maxwidth = 7.5 * 96;
       $('body').css('zoom', maxwidth / $('body').width());
-      if (isChrome) {
-        // Chrome doesn't respect page-break-* styles on table rows/cells,
-        // so wrap the contents of table cells with a block element
-        // ref. Chromium bug #99124, #87828, #59193
-        // should be fixed with Chrome 53
-        var nosplits = $('.nosplitrows td');
-        if (nosplits.length > 0) {
-          nosplits.wrapInner('<div class="nosplit autowrap" />');
-        }
-      }
     }
   };
   var afterPrint = function() {
     $('body').css('zoom', 1);
-    // get the direct children of the wrapper divs.
-    var nosplits = $('div.autowrap >');
-    if (nosplits.length > 0) {
-      nosplits.unwrap();
-    }
   }
 
   if (window.matchMedia) { // chrome, most importantly; also IE10?
