ebd5b466b4717f201bc7f8d57ef0f7441e590d91
Jonathan Prykop <jonathan@freeside.biz>
2016-07-26 15:04:54 -0500

    Revert "RT#38217: Send email when logging conditions are met"
    This reverts commit 5f563d5ac7e6e1e93cca382baa42ee106f3db5a0.

--- FS/FS/Log.pm
+++ FS/FS/Log.pm
@@ -9,7 +9,7 @@ use vars qw(@STACK @LEVELS);
 
 # override the stringification of @_ with something more sensible.
 BEGIN {
-  @LEVELS = qw(debug info warning error critical);
+  @LEVELS = qw(debug info notice warning error critical alert emergency);
 
   foreach my $l (@LEVELS) {
     my $sub = sub {
