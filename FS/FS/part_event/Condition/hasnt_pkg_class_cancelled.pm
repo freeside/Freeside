c76dee5b859e969a9a1610be53122981f5690687
Jonathan Prykop <jonathan@freeside.biz>
2016-08-03 23:04:05 -0500

    RT#42043: Check for cancelled packages with a cancellation date age option [age_newest options]

--- FS/FS/part_event/Condition/hasnt_pkg_class_cancelled.pm
+++ FS/FS/part_event/Condition/hasnt_pkg_class_cancelled.pm
@@ -21,9 +21,16 @@ sub option_fields {
                      'type'     => 'select-pkg_class',
                      'multiple' => 1,
                    },
-    'age'       => { 'label'      => 'Cancellation in last',
-                     'type'       => 'freq',
-                   },
+    'age_newest' => { 'label'      => 'Cancelled more than',
+                      'type'       => 'freq',
+                      'post_text'  => ' ago (blank for no limit)',
+                      'allow_blank' => 1,
+                    },
+    'age'        => { 'label'      => 'Cancelled less than',
+                      'type'       => 'freq',
+                      'post_text'  => ' ago (blank for no limit)',
+                      'allow_blank' => 1,
+                    },
   );
 }
 
@@ -32,11 +39,12 @@ sub condition {
 
   my $cust_main = $self->cust_main($object);
 
-  my $age = $self->option_age_from('age', $opt{'time'} );
+  my $oldest = length($self->option('age')) ? $self->option_age_from('age', $opt{'time'} ) : 0;
+  my $newest = $self->option_age_from('age_newest', $opt{'time'} );
 
-  #XXX test
-  my $hashref = $self->option('pkgclass') || {};
-  ! grep { $hashref->{ $_->part_pkg->classnum } && $_->get('cancel') > $age }
+  my $pkgclass = $self->option('pkgclass') || {};
+
+  ! grep { $pkgclass->{ $_->part_pkg->classnum } && ($_->get('cancel') > $oldest) && ($_->get('cancel') <= $newest) }
     $cust_main->cancelled_pkgs;
 }
 
