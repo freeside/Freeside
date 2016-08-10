a7e54f3bf896037443c3da5d8f699acab0177a6c
Mark Wells <mark@freeside.biz>
2016-08-10 14:53:13 -0700

    look up LRNs for more accurate call rating, #71955

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -5647,6 +5647,13 @@ and customer address. Include units.',
       'rate_low'  => 'Lowest rate first',
     ],
   },
+
+  {
+    'key'         => 'cdr-lrn_lookup',
+    'section'     => 'telephony',
+    'description' => 'Look up LRNs of destination numbers for exact matching to the terminating carrier. This feature requires a Freeside support contract.',
+    'type'        => 'checkbox',
+  },
   
   {
     'key'         => 'brand-agent',
