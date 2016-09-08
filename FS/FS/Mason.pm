9a9f45c82bc7901cfa94b6e64a2e3e4d5b15abff
Mark Wells <mark@freeside.biz>
2016-09-08 11:25:07 -0700

    saved searches, core stuff, #72101

--- FS/FS/Mason.pm
+++ FS/FS/Mason.pm
@@ -413,6 +413,8 @@ if ( -e $addl_handler_use_file ) {
   use FS::olt_site;
   use FS::access_user_page_pref;
   use FS::part_svc_msgcat;
+  use FS::saved_search;
+  use FS::saved_search_option;
   # Sammath Naur
 
   if ( $FS::Mason::addl_handler_use ) {
