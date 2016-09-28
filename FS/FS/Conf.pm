f4e9cb590b7189faead0feb36e46aa60dfc26fae
Ivan Kohler <ivan@freeside.biz>
2016-09-28 12:06:20 -0700

    add previous balance to invoice localization, RT#72714

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -4300,6 +4300,7 @@ and customer address. Include units.',
     'section'     => 'invoice_balances',
     'description' => 'Text for the label of the total previous balance, when it is shown separately. Defaults to "Previous Balance".',
     'type'        => 'text',
+    'per_locale'  => 1,
   },
 
   {
@@ -4307,6 +4308,7 @@ and customer address. Include units.',
     'section'     => 'invoice_balances',
     'description' => 'Text for the label of the total of new charges, when it is shown separately. If invoice_show_prior_due_date is enabled, the due date of current charges will be appended. Defaults to "Total New Charges".',
     'type'        => 'text',
+    'per_locale'  => 1,
   },
 
   {
