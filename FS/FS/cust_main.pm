99e45c99df978410140c0e14f17820d57f9e6b54
Ivan Kohler <ivan@freeside.biz>
2016-12-13 12:39:50 -0800

    Revert "Format email addresses w/Email::Address instead of ad-hoc, fixes issues emailing companies with commas, RT#73241"
    This reverts commit bf6d06efe903dfd4c112fb9eccb98344841b729c.

--- FS/FS/cust_main.pm
+++ FS/FS/cust_main.pm
@@ -32,7 +32,6 @@ use Digest::MD5 qw(md5_base64);
 use Date::Format;
 #use Date::Manip;
 use File::Temp; #qw( tempfile );
-use Email::Address;
 use Business::CreditCard 0.28;
 use FS::UID qw( getotaker dbh driver_name );
 use FS::Record qw( qsearchs qsearch dbdef regexp_sql );
@@ -3572,17 +3571,15 @@ sub contact_list_email {
         # unlike on 4.x, we have a separate list of invoice email
         # destinations.
         # make sure they're not redundant with contact emails
-        $emails{ $contact_email->emailaddress } =
-          Email::Address->new( $contact->firstlast,
-                               $contact_email->emailaddress
-                             )->format;
+        my $dest = $contact->firstlast . ' <' . $contact_email->emailaddress . '>';
+        $emails{ $contact_email->emailaddress } = $dest;
       }
     }
   }
   if ( $and_invoice ) {
     foreach my $email ($self->invoicing_list_emailonly) {
-      $emails{ $email } ||=
-        Email::Address->new( $self->name_short, $email )->format;
+      my $dest = $self->name_short . ' <' . $email . '>';
+      $emails{ $email } ||= $dest;
     }
   }
   values %emails;
