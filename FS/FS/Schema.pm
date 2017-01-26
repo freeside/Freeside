87762d33fc08d6a063bc16edab550bdb42f34c9f
Ivan Kohler <ivan@freeside.biz>
2017-01-25 19:53:28 -0800

    improve performance of self-service usage overview with lots of CDRs, RT#73889

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -5621,6 +5621,7 @@ sub tables_hashref {
       'unique' => [],
       'index' => [ [ 'calldate' ],
                    [ 'src' ], [ 'dst' ], [ 'dcontext' ], [ 'charged_party' ],
+                   [ 'startdate' ],
                    [ 'lastapp' ],
                    ['accountcode'], ['uniqueid'], ['carrierid'], ['cdrid'],
                    [ 'sessionnum' ], [ 'subscriber' ],
