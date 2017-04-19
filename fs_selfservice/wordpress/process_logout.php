a0001672ba855d7402ef891ee3db4488af3e9314
Ivan Kohler <ivan@freeside.biz>
2017-04-19 12:57:58 -0700

    add logout to example wordpress self-service, RT#75279

---
+++ fs_selfservice/wordpress/process_logout.php
@@ -0,0 +1,13 @@
+<?php
+
+$GLOBALS['FREESIDE_PROCESS_LOGOUT'] = true;
+
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+
+get_header();
+
+?>
+
+You have been logged out.
+
+<?php get_footer(); ?>
