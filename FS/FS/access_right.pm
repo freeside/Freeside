1cd83e5112019111a8f6c16eacd5264a95abcfb2
Ivan Kohler <ivan@freeside.biz>
2017-05-15 10:49:38 -0700

    separate edit and delete rights for customer notes, RT#76001

--- FS/FS/access_right.pm
+++ FS/FS/access_right.pm
@@ -258,6 +258,7 @@ sub _upgrade_data { # class method
                                      ],
     'Resend invoices' => 'Print and mail invoices',
     'List customers' => 'Customers: Customer churn report',
+    'Edit customer note' => 'Delete customer note',
   );
 
 #  foreach my $old_acl ( keys %onetime ) {
