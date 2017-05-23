db4224c7e058492c8e32a39d5b6dd3c3ebe01955
Ivan Kohler <ivan@freeside.biz>
2017-05-23 09:47:51 -0700

    don't echo back a password in the error message

--- FS/FS/svc_acct.pm
+++ FS/FS/svc_acct.pm
@@ -1413,8 +1413,7 @@ sub check {
       $recref->{_password} = $1;
     } else {
       return gettext('illegal_password'). " $passwordmin-$passwordmax ".
-             FS::Msgcat::_gettext('illegal_password_characters').
-             ": ". $recref->{_password};
+             FS::Msgcat::_gettext('illegal_password_characters');
     }
 
     if ( $password_noampersand ) {
