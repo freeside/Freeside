5a8140c9027c5c035ac0ed42e053f27231edd0ba
Ivan Kohler <ivan@freeside.biz>
2017-08-25 15:31:56 -0700

    separate access control for payment reports, RT#77007

--- FS/FS/access_right.pm
+++ FS/FS/access_right.pm
@@ -263,6 +263,7 @@ sub _upgrade_data { # class method
     'List customers' => 'Customers: Customer churn report',
     'Edit customer note' => 'Delete customer note',
     'Edit customer' => 'Edit customer invoice terms',
+    'Financial reports' => 'Basic payment and refund reports',
   );
 
 #  foreach my $old_acl ( keys %onetime ) {
