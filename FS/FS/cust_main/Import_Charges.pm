e9bbe01798d8fccf0df50f70cee8b1ded319eb1c
Christopher Burger <burgerc@freeside.biz>
2018-01-26 16:24:28 -0500

    RT# 75095 - updated mapping of ooma import file format

--- FS/FS/cust_main/Import_Charges.pm
+++ FS/FS/cust_main/Import_Charges.pm
@@ -68,7 +68,9 @@ sub batch_charge {
   if ( $format eq 'simple' ) {
     @fields = qw( custnum agent_custid amount pkg );
   } elsif ( $format eq 'ooma' ) {
-    @fields = ( 'userfield1', 'userfield2', 'userfield3', 'userfield4', 'userfield5', 'userfield6', 'userfield7', 'userfield8', 'userfield9', 'userfield10', 'amount', 'userfield12', 'userfield13', 'userfield14', 'userfield15', 'userfield16', 'userfield17', 'userfield18', 'pkg', 'userfield20', 'custnum', 'userfield22', 'userfield23', 'userfield24', 'userfield25', );
+    #@fields = ( 'userfield1', 'userfield2', 'userfield3', 'userfield4', 'userfield5', 'userfield6', 'userfield7', 'userfield8', 'userfield9', 'userfield10', 'amount', 'userfield12', 'userfield13', 'userfield14', 'userfield15', 'userfield16', 'userfield17', 'userfield18', 'pkg', 'userfield20', 'custnum', 'userfield22', 'userfield23', 'userfield24', 'userfield25', );
+    @fields = ( 'userfield1', 'userfield2', 'userfield3', 'userfield4', 'userfield5', 'userfield6', 'userfield7', 'userfield8', 'amount', 'userfield10', 'userfield11', 'userfield12', 'userfield13', 'userfield14', 'userfield15', 'userfield16', 'pkg', 'userfield18', 'custnum', 'userfield20', 'userfield21', 'userfield22', 'userfield23', 'userfield24', 'userfield25', );
+
   ##should charges to charge be a config option?
     %charges = (
       'DISABILITY ACCESS/ENHANCED 911 SERVICES SURCHARGE' => '1',
