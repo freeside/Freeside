e517b3733dd3d19a7af586c9f4b8cf9982a8b3d2
Mitch Jackson <mitch@freeside.biz>
2018-01-27 21:21:59 -0600

    RT# 73421 Add allow messages flag message_dest to contact_email

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -1793,7 +1793,8 @@ sub tables_hashref {
         'classnum',              'int', 'NULL',  '', '', '',
         'comment',           'varchar', 'NULL', 255, '', '',
         'selfservice_access',   'char', 'NULL',   1, '', '',
-        'invoice_dest',         'char', 'NULL',       1, '', '',
+        'invoice_dest',         'char', 'NULL',   1, '', '', # Y or NULL
+        'message_dest',         'char', 'NULL',   1, '', '', # Y or NULL
       ],
       'primary_key'  => 'custcontactnum',
       'unique'       => [ [ 'custnum', 'contactnum' ], ],
