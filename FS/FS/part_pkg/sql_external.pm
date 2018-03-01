48e8d2b117146ea7ad78ada15701226c89ad95d5
Christopher Burger <burgerc@freeside.biz>
2018-03-01 14:38:54 -0500

    RT# 79239 - added option to prorate first month to synchronize with customers other packages with sql_export plan

--- FS/FS/part_pkg/sql_external.pm
+++ FS/FS/part_pkg/sql_external.pm
@@ -19,6 +19,10 @@ our @detail_cols = ( qw(amount format duration phonenum accountcode
   'shortname' => 'External SQL query',
   'inherit_fields' => [ 'prorate_Mixin', 'global_Mixin' ],
   'fields' => {
+    'sync_bill_date' => { 'name' => 'Prorate first month to synchronize '.
+                                    'with the customer\'s other packages',
+                          'type' => 'checkbox',
+                        },
     'cutoff_day'    => { 'name' => 'Billing Day (1 - 28) for prorating or '.
                                    'subscription',
                          'default' => '1',
@@ -50,7 +54,7 @@ our @detail_cols = ( qw(amount format duration phonenum accountcode
     },
 
   },
-  'fieldorder' => [qw( recur_method cutoff_day ),
+  'fieldorder' => [qw( recur_method cutoff_day sync_bill_date),
                    FS::part_pkg::prorate_Mixin::fieldorder,
                    qw( datasrc db_username db_password query query_style
                   )],
@@ -138,6 +142,44 @@ sub calc_recur {
   $param->{'override_quantity'} = $quantity;
   $param->{'override_charges'} = $price;
   ($cust_pkg->quantity || 1) * $self->calc_recur_Common($cust_pkg,$sdate,$details,$param);
+}
+
+sub cutoff_day {
+  my $self = shift;
+  my $cust_pkg = shift;
+  my $cust_main = $cust_pkg->cust_main;
+  # force it to act like a prorate package, is what this means
+  # because we made a distinction once between prorate and flat packages
+  if ( $cust_main->force_prorate_day  and $cust_main->prorate_day ) {
+     return ( $cust_main->prorate_day );
+  }
+  if ( $self->option('sync_bill_date',1) ) {
+    my $next_bill = $cust_pkg->cust_main->next_bill_date;
+    if ( $next_bill ) {
+      return (localtime($next_bill))[3];
+    } else {
+      # This is the customer's only active package and hasn't been billed
+      # yet, so set the cutoff day to either today or tomorrow, whichever
+      # would result in a full period after rounding.
+      my $setup = $cust_pkg->setup; # because it's "now"
+      my $rounding_mode = $self->option('prorate_round_day',1);
+      return () if !$setup or !$rounding_mode;
+      my ($sec, $min, $hour, $mday, $mon, $year) = localtime($setup);
+
+      if (   ( $rounding_mode == 1 and $hour >= 12 )
+          or ( $rounding_mode == 3 and ( $sec > 0 or $min > 0 or $hour > 0 ))
+      ) {
+        # then the prorate period will be rounded down to start from
+        # midnight tomorrow, so the cutoff day should be the current day +
+        # 1.
+        $setup = timelocal(59,59,23,$mday,$mon,$year) + 1;
+        $mday = (localtime($setup))[3];
+      }
+      # otherwise, it will be rounded up, so leave the cutoff day at today.
+      return $mday;
+    }
+  }
+  return ();
 }
 
 sub can_discount { 1; }
