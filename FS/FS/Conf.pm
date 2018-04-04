11bbf29de447fe39e9d7155fe280a0df70fa8c3c
Ivan Kohler <ivan@freeside.biz>
2018-04-04 12:49:49 -0700

    invoice_sections_with_taxes per-agent, RT#79636

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -1581,6 +1581,7 @@ and customer address. Include units.',
     'description' => 'Split invoice into sections and label according to either package category or location when enabled.',
     'type'        => 'checkbox',
     'per_agent'   => 1,
+    'config_bool' => 1,
   },
 
   {
@@ -1612,6 +1613,8 @@ and customer address. Include units.',
     'section'     => 'invoicing',
     'description' => 'Include taxes within each section of mutli-section invoices.',
     'type'        => 'checkbox',
+    'per_agent'   => 1,
+    'agent_bool'  => 1,
   },
 
   {
