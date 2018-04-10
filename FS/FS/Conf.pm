16c47fa28ef012f3000359937465b10539ec76df
Ivan Kohler <ivan@freeside.biz>
2018-04-10 16:03:04 -0700

    invoice_usesummary is already agent virt, just need to be able to set it, RT#79636

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -1230,6 +1230,7 @@ my $validate_email = sub { $_[0] =~
     'section'     => 'invoicing',
     'description' => 'Indicates that html and latex invoices should be in summary style and make use of invoice_latexsummary.',
     'type'        => 'checkbox',
+    'per_agent'   => 1,
   },
 
   {
