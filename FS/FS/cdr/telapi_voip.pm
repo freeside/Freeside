121bbbde83b22ae0837d8010e743dc83396a30f4
Christopher Burger <burgerc@freeside.biz>
2018-04-11 07:51:23 -0400

    RT# 75680 - Added date parse, and display insert error.

--- FS/FS/cdr/telapi_voip.pm
+++ FS/FS/cdr/telapi_voip.pm
@@ -26,8 +26,10 @@ use FS::cdr qw( _cdr_date_parser_maker _cdr_min_parser_maker );
       my($cdr, $cdrtypename, $conf, $param) = @_;
       return unless length($cdrtypename);
       _init_cdr_types();
-      die "no matching cdrtypenum for $cdrtypename"
-        unless defined $CDR_TYPES->{$cdrtypename};
+      unless (defined $CDR_TYPES->{$cdrtypename}) {
+        warn "Skipping Record: CDR type name $cdrtypename does not exist!";
+        $param->{skiprow} = 1;
+      }
       $cdr->cdrtypenum($CDR_TYPES->{$cdrtypename});
     },                                   # type 
     _cdr_min_parser_maker('billsec'),     #PriceDurationMins
