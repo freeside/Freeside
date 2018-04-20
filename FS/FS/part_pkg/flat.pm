b8aea2bf8dcebf7a6b9a217ee78114e42404fe68
Christopher Burger <burgerc@freeside.biz>
2018-04-20 12:08:43 -0400

    RT# 77964 - fixed so deferring date now works when waive setup fee is set

--- FS/FS/part_pkg/flat.pm
+++ FS/FS/part_pkg/flat.pm
@@ -118,22 +118,27 @@ sub calc_setup {
 
   return 0 if $self->prorate_setup($cust_pkg, $sdate);
 
-  my $i = 0;
-  my $count = $self->option( 'additional_count', 'quiet' ) || 0;
-  while ($i < $count) {
-    push @$details, $self->option( 'additional_info' . $i++ );
-  }
+  if (!$cust_pkg->waive_setup) {
+    my $i = 0;
+    my $count = $self->option( 'additional_count', 'quiet' ) || 0;
+    while ($i < $count) {
+      push @$details, $self->option( 'additional_info' . $i++ );
+    }
 
-  my $charge = $self->base_setup($cust_pkg, $sdate, $details);
+    my $charge = $self->base_setup($cust_pkg, $sdate, $details);
 
-  my $discount = 0;
-  if ( $charge > 0 ) {
+    my $discount = 0;
+    if ( $charge > 0 ) {
       $param->{'setup_charge'} = $charge;
       $discount = $self->calc_discount($cust_pkg, $sdate, $details, $param);
       delete $param->{'setup_charge'};
+    }
+
+    return sprintf( '%.2f', ($cust_pkg->quantity || 1) * ($charge - $discount) );
   }
 
-  sprintf( '%.2f', ($cust_pkg->quantity || 1) * ($charge - $discount) );
+  return;
+
 }
 
 sub base_setup {
