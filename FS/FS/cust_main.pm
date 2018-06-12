b4647550d33068067925f7f3fe8d6fe4f02a67e4
Christopher Burger <burgerc@freeside.biz>
2018-06-12 09:51:58 -0400

    RT# 74435 - fixed errors in posting a echeck refund when no account is listed.
    Conflicts:
    	FS/FS/cust_main/Billing_Batch.pm
    	httemplate/elements/tr-select-cust_payby.html
    	httemplate/misc/payment.cgi
    	httemplate/misc/process/payment.cgi

--- FS/FS/cust_main.pm
+++ FS/FS/cust_main.pm
@@ -2790,7 +2790,7 @@ sub batch_card {
   } );
 
   foreach (qw( address1 address2 city state zip country latitude longitude
-               payby payinfo paydate payname paycode ))
+               payby payinfo paydate payname paycode paytype ))
   {
     $options{$_} = '' unless exists($options{$_});
   }
@@ -2817,7 +2817,7 @@ sub batch_card {
     'exp'      => $options{paydate}  || $self->paydate,
     'payname'  => $options{payname}  || $self->payname,
     'amount'   => $amount,                         # consolidating
-    'paycode'  => $options{paycode}  || $cust_payby->paycode,
+    'paycode'  => $options{paycode}  || '',
   } );
   
   $cust_pay_batch->paybatchnum($old_cust_pay_batch->paybatchnum)
