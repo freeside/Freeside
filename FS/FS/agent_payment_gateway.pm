06a55e88c46108a667d2c26e59b210382a9ea984
Christopher Burger <burgerc@freeside.biz>
2019-01-08 11:59:51 -0500

    Revert "RT# - updated payment gateway override to not drop ACH overrides on upgrade"
    This reverts commit 8ea0bfff9308f4c13d170c45cbac6ba7a8a9e488.

--- FS/FS/agent_payment_gateway.pm
+++ FS/FS/agent_payment_gateway.pm
@@ -119,10 +119,10 @@ sub _upgrade_data {
       'table' => 'agent_payment_gateway',
       'extra_sql' => ' WHERE taxclass IS NOT NULL AND taxclass != \'\'',
     });
-  die "Non ACH (E-check) Agent cardtype override no longer supported"
+  die "Agent cardtype override no longer supported"
     if qsearch({
       'table' => 'agent_payment_gateway',
-      'extra_sql' => ' WHERE cardtype IS NOT NULL AND cardtype != \'\' AND cardtype != \'ACH\'',
+      'extra_sql' => ' WHERE cardtype IS NOT NULL AND cardtype != \'\'',
     });
   return '';
 }
