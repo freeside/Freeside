20961c94ea06ad3aa71fad2bd7f6903f83f02ebc
Ivan Kohler <ivan@freeside.biz>
2021-03-17 19:28:00 -0700

    disable fcc 477 deployment zones, RT#85668

--- FS/FS/Schema.pm
+++ FS/FS/Schema.pm
@@ -7450,6 +7450,7 @@ sub tables_hashref {
         'is_business',    'char',    'NULL', 1,       '', '',
         'active_date',    @date_type,                 '', '',
         'expire_date',    @date_type,                 '', '',
+        'disabled',       'char',    'NULL',       1, '', '',
       ],
       'primary_key' => 'zonenum',
       'unique' => [],
